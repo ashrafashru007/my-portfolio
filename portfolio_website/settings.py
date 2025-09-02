8d34c1c45ac5a693b28f0c4f9b73c8eb06289b8d
Mohammed Ashraf B R <mohammedashrafbr@Ashrus-MacBook-Air.local>
2025-09-02 14:47:38 +0530

    Fix ALLOWED_HOSTS for Render

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s^7m7d0&2093v1h9bgz!m8r-$8#n4x#aec$i3j+ian_z68w1!t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mohammedashrafbr.onrender.com", "127.0.0.1", "localhost"]
+
 
 
 # Application definition
