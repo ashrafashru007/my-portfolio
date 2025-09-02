32a9d75cd9df5da87f978547d4803a96426a4577
Mohammed Ashraf B R <mohammedashrafbr@Ashrus-MacBook-Air.local>
2025-09-02 14:28:51 +0530

    Added Whitenoise for static files on Render

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'portfolio_website.urls'
@@ -132,3 +133,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
